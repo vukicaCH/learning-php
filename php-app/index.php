28109ebb8a08f5a4ec22fbba084f44c969a2d6ef
Vuk Trifunovic <67346034+vukicaCH@users.noreply.github.com>
2025-10-24 14:39:37 +0200

    Update index.php

--- php-app/index.php
+++ php-app/index.php
@@ -4,13 +4,10 @@ require "functions.php";
 
 require "router.php";
 
-$dsn = "mysql:host=localhost;port=3306;dbname=php_test;";
-
-$pdo = new PDO($dsn, 'root');
-
 $statement = $pdo->prepare("select * from posts");
 $statement->execute();
 
 $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
 
 dd($posts);
